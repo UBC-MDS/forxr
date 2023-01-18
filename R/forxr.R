ed213bcc373cd06fd181677ea8e341ba420f8627
stepanz25 <112679383+stepanz25@users.noreply.github.com>
2023-01-18 09:39:29 -0800

    edited the docstring

--- R/forxr.R
+++ R/forxr.R
@@ -1,13 +1,13 @@
-#' Retrieve historical daily currency exchange rate data for Canadian Dollars in CSV format from the Bank of Canada website.
+#' Retrieve historical daily currency exchange rates data for Canadian Dollar in CSV format from Bank of Canada website.
+#' The function pre-processes and cleans the data to transform it into a more usable format.
+#' 
+#' @param output if the value is False then only display the data frame, if the value is True then write the file to the current working directory 
 #'
-#' @param start_date inputted starting date in the format specified '%YYYY-%mm-%dd'
-#' @param output_path the path where the csv file will get written to
-#'
-#' @return csv data file that includes historical data on currency exchange rates
+#' @return cleaned and processed csv data file that includes historical data on currency exchange rates
 #' @export
 #'
 #' @examples
-#' retrieve_data('2022-05-23', 'my_document/currency_exchange.csv')
-retrieve_data <- function(start_date, output_path) {
-  print("My function")
+#' retrieve_data(output)
+retrieve_data <- function(output) {
+  print("retrieve_data_function")
   }
