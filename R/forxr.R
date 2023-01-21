1739999993e7cdef430e51148a52aedab6cb2afe
Hongjian Li <samli016@student.ubc.ca>
2023-01-21 14:13:19 -0800

    add function convert

--- R/forxr.R
+++ R/forxr.R
@@ -11,3 +11,19 @@
 retrieve_data <- function(output) {
   print("retrieve_data_function")
   }
+
+#' Takes a currency value and the currency type
+#'to be converted to as input and returns the converted currency
+#'value as per the current conversion rate.
+#' @param value The value of the original currency to be converted
+#' @param currency1 The type of currency originally
+#' @param currency2 The type of currency that the currency1 will be converted to
+#'
+#' @return Returns converted numeric currency
+#' @export
+#'
+#' @examples
+#' currency_convert(23, 'USD', 'CAD')
+currency_convert <- function(value, currency1, currency2) {
+  print("TBD")
+}
